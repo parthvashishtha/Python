20eb1bed35a40cf600528fa362415d7ffe194746
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-08 21:32:26 +0530

    Update operators.py

--- Py.learning_files/operators.py
+++ Py.learning_files/operators.py
@@ -1,1 +1,36 @@
+a=12
+b=5
+
+# Arithmetic Operations
+
+print("The value of 12+5 is : ", 12+5)
+print("The value of 12-5 is : ", 12-5)
+print("The value of 12*5 is : ", 12*5)
+print("The value of 12/5 is : ", 12/5)
+
+# Assinments operators
+
+a=14
+a+=2
+a-=5
+a*=3
+a/=10
+print(a)
+
+# Comparison Operators 
+
+c= (12>3)
+c= (12>=3)
+c= (12<=3)
+c= (12<3)
+print(c)
+
+# Logical operators 
+
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is :",bool1 and bool2)
+print("The value of bool1 or bool2 is :",bool1 or bool2)
+print("The value of not bool1  is :",not bool1 )
+
 
