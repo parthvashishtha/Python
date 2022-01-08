f68f575683def8b2e92248a08b7e8d18d0498b03
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-08 21:28:44 +0530

    Update playsound.py

--- Py.learning_files/playsound.py
+++ Py.learning_files/playsound.py
@@ -1,1 +1,4 @@
+from playsound import playsound
+playsound('C:\\Users\\Acer\\Documents\\Python_Programming\\play.mp3.mp3')
+# here playsound is an external module which is downloaded using pip(Pip installs Packages) using pip install playsound command.
 
