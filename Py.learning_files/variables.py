a0c05d137f38181f75ee7409ee8b510559ae2f57
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-08 21:31:05 +0530

    Update variables.py

--- Py.learning_files/variables.py
+++ Py.learning_files/variables.py
@@ -1,1 +1,14 @@
+name='Parth Sharma'
+age= 19
+dob= 2002
+height= 180.5
 
+print(name)
+print(age)
+print(dob)
+print(height)
+
+print(type(name))
+print(type(age))
+print(type(dob))
+print(type(height))
