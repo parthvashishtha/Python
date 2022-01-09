821b8beb5b86f071a8c5f4c79f947ed14b109966
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 21:50:49 +0530

    Update tuples.py

--- Py.learning_files/tuples.py
+++ Py.learning_files/tuples.py
@@ -1,1 +1,13 @@
+# Creating a tuple using ()
+t = (1,3,5,2)
+print(t)
 
+# Cannot update a tuple
+t[1]=20
+print(t)
+
+# while assigning only one element to tuple use a comma
+t2 = (1) # wrong way
+print(t2)
+t2 =(1,)
+print(t2)
