92c38e59b19aaf923dc7ac3ab4c36ecf1f06b79e
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 20:37:23 +0530

    Update Messageprint.py

--- Py.learning_files/Basic_practice_programs/Messageprint.py
+++ Py.learning_files/Basic_practice_programs/Messageprint.py
@@ -1,1 +1,12 @@
+Letter= ''' Dear <|NAME|> 
+ You are selected!
+ Date : <|DATE|> '''
 
+name = input("Enter your name:\n")
+date= input("Enter the date:\n ")
+
+Letter= Letter.replace("<|NAME|>",name)
+Letter= Letter.replace("<|DATE|>",date)
+
+print (Letter)
+       
