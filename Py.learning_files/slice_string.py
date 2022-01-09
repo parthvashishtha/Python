39e4921520427feab14b11616f5d4fef592216ca
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 20:06:34 +0530

    Update slice_string.py

--- Py.learning_files/slice_string.py
+++ Py.learning_files/slice_string.py
@@ -1,1 +1,25 @@
+greeting= "Hello Parth"
+print(greeting)
+b= " Good Morning"
+# Concatenating two  strings
+c= greeting + b
+print(c)
+
+b = "Parth"
+
+# methods of printing a part of a string(numbers represent a sort of range )
+
+print(b[3])
+print(b[1:4])
+print(b[:4])
+print(b[1:])
+print(b[:])
+print(b[-5:])
+print(b[-4:-1])
+
+# third one used for skipping every third  leter below 
+
+b= "ParthISaGoodBoy"
+print(b[1:10:3])
+
 
