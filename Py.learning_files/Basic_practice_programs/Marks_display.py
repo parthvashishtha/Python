57eaf30a7a6bc0e693880875cfe518d244d163f3
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 21:53:25 +0530

    Update Marks_display.py

--- Py.learning_files/Basic_practice_programs/Marks_display.py
+++ Py.learning_files/Basic_practice_programs/Marks_display.py
@@ -1,1 +1,10 @@
+a1 = input("Enter marks of 1st student :\n ")
+a2 = input("Enter marks of 2nd student :\n ")
+a3 = input("Enter marks of 3rd student :\n ")
+a4 = input("Enter marks of 4th student :\n ")
+a5 = input("Enter marks of 5th student :\n ")
+a6 = input("Enter marks of 6th student :\n ")
 
+Marks_list = [a1,a2,a3,a4,a5,a6]
+Marks_list.sort()
+print(Marks_list)
