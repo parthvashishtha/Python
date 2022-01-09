1127751ceb456d7878193ebc91d74be0d28b5b06
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 21:48:45 +0530

    Update List.py

--- Py.learning_files/List.py
+++ Py.learning_files/List.py
@@ -1,1 +1,18 @@
+# Create a list
 
+a = [1,2,5,45,50]
+print(a)
+
+# Changing element
+
+a[1]= 10
+print(a)
+
+# Access using index number
+
+print(a[3])
+
+# Can create list using different items
+
+b= ["Parth", 19, 20000000, "Ram"]
+print(b)
