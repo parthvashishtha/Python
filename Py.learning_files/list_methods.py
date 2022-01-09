f53cfef895111082d3465f5890abb071445be2bb
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 21:50:11 +0530

    Update list_methods.py

--- Py.learning_files/list_methods.py
+++ Py.learning_files/list_methods.py
@@ -1,1 +1,12 @@
+l1=[2,3,5,23,12,20]
+print(l1)
 
+#  Contains methods 
+#  l1.sort()# 1. Sort
+#  l1.reverse() # reverse
+#  l1.append(45) # adds element at the end
+#  l1.pop(2) # removes element from the index
+#  l1.remove(20) # removes the given element
+
+
+print(l1)
