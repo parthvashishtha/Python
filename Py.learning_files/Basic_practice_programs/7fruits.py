e7dd83f4bf0e58e514ac100a458d2192a9b75d21
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-09 21:52:41 +0530

    Update 7fruits.py

--- Py.learning_files/Basic_practice_programs/7fruits.py
+++ Py.learning_files/Basic_practice_programs/7fruits.py
@@ -1,1 +1,10 @@
+fru = ["a","b","c","d","e","f","g"]
+fru[0] = input("Enter first fruit :\n ")
+fru[1] = input("Enter second fruit :\n ")
+fru[2] = input("Enter third fruit :\n ")
+fru[3] = input("Enter fourth fruit :\n ")
+fru[4] = input("Enter fifth fruit :\n ")
+fru[5] = input("Enter sixth fruit :\n ")
+fru[6] = input("Enter seventh fruit :\n ")
 
+print(fru)
