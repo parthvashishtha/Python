a7ecb5c914e0275a4e38e23935ac0592dbc41036
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-10 23:07:04 +0530

    Update Dictionary_syntax.py

--- Py.learning_files/Dictionary_syntax.py
+++ Py.learning_files/Dictionary_syntax.py
@@ -1,1 +1,13 @@
 
+myDict = {
+    "laptop" : "An electronic machine",
+    "parth " : "A simple boy",
+    "number" : [1,3,5],
+    "anotherDict" : {"Parth": "Coder"}
+}
+
+print(myDict["Laptop"])
+print(myDict["Number"])
+# It's an example of nested key
+# Dictionary - Key:Value
+print(myDict["anotherDict"]["Parth"])
