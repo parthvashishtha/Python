c6dffd4f7d6075d394e021ac37a549c5e105cdfb
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 19:53:28 +0530

    Update GreatestOf4.py

--- Py.learning_files/Basic_practice_programs/GreatestOf4.py
+++ Py.learning_files/Basic_practice_programs/GreatestOf4.py
@@ -1,1 +1,31 @@
+a = int(input("Enter first  number : \n"))
+b = int(input("Enter second number : \n"))
+c = int(input("Enter third  number : \n"))
+d = int(input("Enter fourth number : \n"))
+
+# if(a>b and a>c and a>d):
+#     print("Greatest number is : ",a)
+# if(b>c and b>d and b>a):
+#     print("Greatest number is : ",b)
+# if(c>a and c>b and c>d):
+#     print("Greatest number is : ",c)
+# if(d>a and d>b and d>c):
+#     print("Greatest number is : ",d)
+ 
+# Second method
+
+if a>d:
+    f1=a
+else:
+    f1=d;    
+if b>c:
+    f2=c
+else:
+    f2=b;
+
+if f1>f2:
+    print(str(f1) + " is the greatest number ")   
+else: 
+    print(str(f2) + " is the greatest number ")   
+   
 
