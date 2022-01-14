04ded48dc5dbd5f2d42bc65e26006945d7a40e3e
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 19:54:16 +0530

    Update Marks_PassOrfail.py

--- Py.learning_files/Basic_practice_programs/Marks_PassOrfail.py
+++ Py.learning_files/Basic_practice_programs/Marks_PassOrfail.py
@@ -1,1 +1,13 @@
+print("Scores of Student")
+
+m = int(input("Enter the score of Maths\n"))
+e = int(input("Enter the score of English\n"))
+h = int(input("Enter the score of Hindi\n"))
+
+if(m/100>=0.33 and e/100>=0.33 and h/100>=0.33 and (m+e+h)/100>=0.40):
+    print("Pass")
+    
+else:
+    print("Fail")    
+
 
