2aee5f0c9330bc84d8434103233aaa53a9e09915
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 17:36:56 +0530

    Update UniqueNumPrint.py

--- Py.learning_files/Basic_practice_programs/UniqueNumPrint.py
+++ Py.learning_files/Basic_practice_programs/UniqueNumPrint.py
@@ -1,1 +1,11 @@
+num1 =  int(input("Enter number 1 you want to print:\n "))
+num2 =  int(input("Enter number 2 you want to print:\n "))
+num3 =  int(input("Enter number 3 you want to print:\n "))
+num4 =  int(input("Enter number 4 you want to print:\n "))
+num5 =  int(input("Enter number 5 you want to print:\n "))
+num6 =  int(input("Enter number 6 you want to print:\n "))
+num7 =  int(input("Enter number 7 you want to print:\n "))
+num8 =  int(input("Enter number 8 you want to print:\n "))
 
+s= {num1, num2 , num3 , num4 , num5 , num6, num7 ,num8}
+print(s)
