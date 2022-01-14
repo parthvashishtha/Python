07fdb7dc7b6ac7ce4ac2e06690284945bebfaa67
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 17:37:44 +0530

    Update Friends_dict.py

--- Py.learning_files/Basic_practice_programs/Friends_dict.py
+++ Py.learning_files/Basic_practice_programs/Friends_dict.py
@@ -1,1 +1,12 @@
+friDict = {}
+a = input("Enter your favourite language Lakshya:\n")
+b = input("Enter your favourite language Madhur:\n")
+c = input("Enter your favourite language Priyans\n")
+d = input("Enter your favourite language Aryan:\n")
 
+friDict["Lakshya"] = a
+friDict["Madhur"] = b
+friDict["Priyans"] = c
+friDict["Aryan"] = d
+
+print(friDict)
