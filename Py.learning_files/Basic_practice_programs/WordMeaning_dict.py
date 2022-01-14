813428de298c7220afea2fe9879d87162da75d2a
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 17:35:56 +0530

    Update WordMeaning_dict.py

--- Py.learning_files/Basic_practice_programs/WordMeaning_dict.py
+++ Py.learning_files/Basic_practice_programs/WordMeaning_dict.py
@@ -1,1 +1,11 @@
+mydict = {
+      "Duniya":"World",
+      "Ram"   :"Hindu God",
+      "Dil"   :"Heart"
 
+}
+print("Given words are",mydict.keys())
+a = input("Enter the word whose meaning you want to know\n")
+# print("Meaning of this word is:",mydict[a])
+# Below line will not throw an error
+print("Meaning of this word is:",mydict.get(a))
