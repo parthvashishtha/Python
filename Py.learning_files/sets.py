7ddf6be27374bf54c3cab527ab786c0e9a0ed7cd
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 17:34:09 +0530

    Update sets.py

--- Py.learning_files/sets.py
+++ Py.learning_files/sets.py
@@ -1,1 +1,17 @@
+a = {1, 2 , 3, 4}
+print(type(a))
+
+# It's a collection of non empty elements
+# As shown below it will ignore 4
+
+a = {1,2,3,4,5,4}
+print(a)
+
+b = {} # This syntax will create an empty dictionary not an empty set
+print(type(b))
+
+# For an empty set we have to use this
+c = set()
+print(type(c))
+
 
