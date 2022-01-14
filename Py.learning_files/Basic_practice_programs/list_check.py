a36aa5d89a5c3c22d28ec6cdbd85a56b3131c96c
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 19:55:37 +0530

    Update list_check.py

--- Py.learning_files/Basic_practice_programs/list_check.py
+++ Py.learning_files/Basic_practice_programs/list_check.py
@@ -1,1 +1,7 @@
+Cars = ["pulse","swift","duster","ertiga"]
+car_name = input("Enter the car name: \n")
 
+if car_name in Cars:
+    print("This car is in the list ")
+else:
+    print("This car is not  in the list")    
