d9107d5d604f7a98934ef0a018a9e42e898fceb7
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 19:52:00 +0530

    Update logical_relations.py

--- Py.learning_files/logical_relations.py
+++ Py.learning_files/logical_relations.py
@@ -1,1 +1,18 @@
 
+
+a = int(input("Enter your age: "))
+#  Use of and operator 
+if (a>15 and a<40):
+    print("You are welcome :) ")
+
+else:
+    print("Get lost !!")    
+
+# Use of or operator
+
+if(a>15 or a<40):
+    print("You are welcome :) ")
+
+else:
+    print("Get lost !!") 
+
