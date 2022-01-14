57ec81a9604b24de23fd5f3aeb5494675d72a4d7
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-14 19:54:56 +0530

    Update spam_prog.py

--- Py.learning_files/Basic_practice_programs/spam_prog.py
+++ Py.learning_files/Basic_practice_programs/spam_prog.py
@@ -1,1 +1,20 @@
+text = input(" Enter the text\n")
 
+
+
+if("make a lot of money" in text):
+   spam = True
+elif("buy now" in text):
+    spam = True
+elif("click this" in text):
+    spam = True
+elif("subscribe this" in text):
+    spam = True
+
+
+if (spam):
+    print("This text is spam")
+else:
+    print("This text is not spam")      
+
+    
