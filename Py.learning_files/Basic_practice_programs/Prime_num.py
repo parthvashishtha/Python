eb403a1a37e98f470356f38d5fd9cc2567dd1e00
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-16 01:38:32 +0530

    Update Prime_num.py

--- Py.learning_files/Basic_practice_programs/Prime_num.py
+++ Py.learning_files/Basic_practice_programs/Prime_num.py
@@ -1,1 +1,14 @@
 
+num = int(input("Enter the number: \n"))
+prime = True
+for i  in range(2,num):
+    if (num%i==0):
+        prime= False
+
+if prime:
+    print( "Given no. is prime")     
+else:
+    print( "Given no. is not prime")    
+
+
+
