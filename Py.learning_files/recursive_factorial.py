ae6f93ccb47322bf9fb177df67206dbcda233f7b
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-17 22:26:16 +0530

    Update recursive_factorial.py

--- Py.learning_files/recursive_factorial.py
+++ Py.learning_files/recursive_factorial.py
@@ -1,1 +1,16 @@
+# Used to print factorial
+# n = 4
+# fact = 1
+# for i in range(n):
+#     fact=fact*(i+1)
 
+# print(fact)
+
+def factorial_recursive(n):
+    if n==1 or n==0:
+     return 1
+
+    return n*factorial_recursive(n-1)
+
+f= factorial_recursive(6) 
+print(f)   
