6de0e4ea84537297cb6531534a639483cd469abd
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-18 20:51:55 +0530

    Update Max0f3_functions.py

--- Py.learning_files/Basic_practice_programs/Max0f3_functions.py
+++ Py.learning_files/Basic_practice_programs/Max0f3_functions.py
@@ -1,1 +1,15 @@
+def Max(num1,num2,num3):
+    if(num1>num2):
+       if(num1>num3):
+           return num1
 
+    else:
+      
+        if(num2>num3):
+           return num2
+
+        else:
+          return num3
+
+m = Max(12,35,40)
+print("Maximum of these three is: ",str(m))                  
