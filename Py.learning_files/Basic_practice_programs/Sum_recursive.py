cbbe8232ee8da13f5d1ddadda2d256c0cd2a1d94
parthvashishtha <86569622+parthvashishtha@users.noreply.github.com>
2022-01-18 20:52:39 +0530

    Update Sum_recursive.py

--- Py.learning_files/Basic_practice_programs/Sum_recursive.py
+++ Py.learning_files/Basic_practice_programs/Sum_recursive.py
@@ -1,1 +1,8 @@
+def sum_recursive(n):
+    if n==0:
+        return 0
+    return n+sum_recursive(n-1)
 
+n = input("Enter the number upto which you want the sum : ")
+t=(sum_recursive(5)) 
+print(f"Sum upto {n} is : ", str(t) )   
